6a3e61fdcc532b8e5e6b8e354ea47449105de7ab
Austin <awoods7753@gmail.com>
2022-03-01 13:54:37 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (drivers, name) {
+    let names = drivers.filter( obj => obj.toLowerCase() === name.toLowerCase())
+    return names;
+}
+
+function fuzzyMatch (drivers, firsttwoletters) {
+    let strings = drivers.filter ( obj => obj.slice(0,2) === firsttwoletters.slice(0,2));
+    if (strings) {
+    return strings;
+    }
+}
+
+function matchName (drivers, x) {
+    let property = drivers.filter (obj => obj.name === x)
+        if (property) {
+        return property;
+        }
+    }
